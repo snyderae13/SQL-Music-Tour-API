41b9de489adc9861fed7b75361041b7535ec2ffc
snyderae13 <snyderae13@icloud.com>
2022-02-14 21:20:37 -0600

    completed and everything is working

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,6 +1,6 @@
 const events = require('express').Router()
 const db = require('../models')
-const { Event } = db
+const { Event, MeetGreet, SetTime, Stage, Band } = db
 const { Op } = require('sequelize')
 
 // FIND ALL EVENTS
@@ -19,10 +19,33 @@ events.get('/', async (req, res) => {
 })
 
 // FIND A SPECIFIC EVENT
-events.get('/:id', async (req, res) => {
+events.get('/:name', async (req, res) => {
     try {
         const foundEvent = await Event.findOne({
-            where: { event_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                {
+                    model: MeetGreet,
+                    as: "meet_greets",
+                    include: {
+                        model: Band,
+                        as: "band",
+                    }
+                },
+                {
+                    model: SetTime,
+                    as: "set_times",
+                    include: [
+                        {model: Band, as: "band"},
+                        {model : Stage, as: "stage"}
+                    ]
+                },
+                {
+                    model: Stage,
+                    as: "stages",
+                    through: {attributes: []}
+                }
+            ]
         })
         res.status(200).json(foundEvent)
     } catch (error) {
--- models/band.js
+++ models/band.js
@@ -9,8 +9,16 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({MeetGreet, SetTime}) {
+      Band.hasMany(MeetGreet, {
+        foreignKey: "band_id",
+        as: "meet_greets"
+      })
+
+      Band.hasMany(SetTime, {
+        foreignKey: "band_id",
+        as: "set_times"
+      })
     }
   }
   Band.init({
